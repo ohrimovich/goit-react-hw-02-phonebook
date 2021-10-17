ee9f76e45958e8f665d64f7507cd2d4bf74101cf
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-10-17 15:32:25 +0300

    added state to local storage

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Filter from "./components/Filter";
 class App extends Component {
  
  state = {
-   contacts: [  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+   contacts: [],
    filter: '',
  }
   
@@ -53,6 +50,17 @@ class App extends Component {
       
     }))
 
+  }
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'))
+    this.setState({contacts: parsedContacts})
+  }
+
+  componentDidUpdate(prevProps,prevState ) {
+    if (this.state.contacts !== prevState.contact) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
   }
 
   render() {
